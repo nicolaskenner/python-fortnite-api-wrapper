3888f445227a17700f93b01a272b834f4f6fe2f7
Nicolas Kenner <18199102+nicolaskenner@users.noreply.github.com>
2018-04-03 14:47:04 -0600

    Update setup.py
    Using a setup.py derived from the official packaging guide

--- setup.py
+++ setup.py
@@ -1,78 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
+here = path.abspath(path.dirname(__file__))
 
-import io
-import os
-import sys
-from shutil import rmtree
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-from setuptools import find_packages, setup
-
-# Package meta-data.
-NAME = 'fortnite'
-DESCRIPTION = 'The Python Fortnite API Wrapper'
-URL = 'https://github.com/nicolaskenner/python-fortnite-api-wrapper'
-EMAIL = 'nick@nicolaskenner.com'
-AUTHOR = 'Nicolas Kenner & Douile'
-REQUIRES_PYTHON = '>=3'
-VERSION = '0.0.2'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'requests'
-]
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    include_package_data=True,
-    license='MIT',
+    name='fortnite',
+    version='0.0.2',
+    description='The Python Fortnite API Wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nicolaskenner/python-fortnite-api-wrapper',
+    author='Nicolas Kenner & Douile',
+    author_email='nick@nicolaskenner.com',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: 3'
     ],
-    # $ setup.py publish support.
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests']
 )
