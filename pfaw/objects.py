c9ccedadc3700eff063eea0cfb44d1c569f966ff
Tom <25043847+Douile@users.noreply.github.com>
2018-04-03 17:42:16 +0100

    Add shop endpoint

--- pfaw/objects.py
+++ pfaw/objects.py
@@ -54,6 +54,84 @@ class BattleRoyale(object):
                     self.top12 += i['value']
                 elif 'placetop25' in i['name']:
                     self.top25 += i['value']
-
     def __str__(self):
         return f'Score: {self.score}\nMatches played: {self.matches}\nMinutes played: {self.time}\nKills: {self.kills}\nWins: {self.wins}\nTop 3: {self.top3}\nTop 5: {self.top5}\nTop 6: {self.top6}\nTop 10: {self.top10}\nTop 12: {self.top12}\nTop 25: {self.top25}'
+
+class Shop(object):
+    def __init__(self, response):
+        storefronts = False
+        self.storefronts = {}
+        for name in response:
+            value = response[name]
+            if name == 'dailyPurchaseHrs':
+                self.dailyPurchaseHours = value
+            elif name == 'expiration':
+                self.expiration = value
+            elif name == 'refreshIntervalHrs':
+                self.refreshIntervalHours = value
+            elif name == 'storefronts':
+                storefronts = True
+        if storefronts:
+            for front in response['storefronts']:
+                storefront = object.Storefront(front)
+                self.storefronts[storefront.name] = storefront
+
+class Storefront(object):
+    def __init__(self, storefront):
+        entries = False
+        self.entries = {}
+        for name in storefront:
+            value = storefront[name]
+            if name == 'name':
+                self.name = value
+            elif name == 'catalogEntries':
+                entries = True
+        if entries:
+            for entry in storefront['catalogEntries']:
+                catalogEntry = object.CatalogEntry(entry)
+                self.entries[catalogEntry.name] = catalogEntry
+                
+class CatalogEntry(object):
+    def __init__(self, entry):
+        prices = False
+        self.prices = {}
+        for name in entry:
+            value = entry[name]
+            if name == 'title':
+                self.title = value
+            elif name == 'devName':
+                self.devName = value
+            elif name == 'description':
+                self.description = value
+            elif name == 'dailyLimit':
+                self.dailyLimit = value
+            elif name == 'monthlyLimit':
+                self.monthlyLimit = value
+            elif name == 'offerId':
+                self.offerId = value
+            elif name == 'offerType':
+                self.offerType = value
+            elif name == 'refundable:
+                self.refundable = value
+            elif name == 'prices':
+                prices = True
+        if prices:
+            for price in entry['prices']:
+                cprice = object.Price(price)
+                self.prices[cprice.currencyType] = cprice
+                
+class Price(object):
+    def __init__(self, price):
+        for name in price:
+            value = price[name]
+            if name == 'currencyType':
+                self.currencyType = value
+            elif name == 'basePrice':
+                self.basePrice = value
+            elif name == 'regularPrice':
+                self.regularPrice = value
+            elif name == 'finalPrice':
+                self.finalPrice = value
+            elif name == 'saleExpiration':
+                self.expiration = value
+                
