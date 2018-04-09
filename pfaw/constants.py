5ea2a387eb040b69029e881ea277095f660bce33
Tom <25043847+Douile@users.noreply.github.com>
2018-04-09 23:18:26 +0100

    Update constants.py

--- pfaw/constants.py
+++ pfaw/constants.py
@@ -5,3 +5,5 @@ battle_royale = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnit
 status = 'https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite'
 friends = 'https://friends-public-service-prod06.ol.epicgames.com/friends/api/public/friends/{}'
 shop = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/catalog?rvn={}'
+news = 'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game'
+profile = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{}/client/QueryProfile?profileId=athena&rvn={}'
