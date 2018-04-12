127fa7422a8eaa040623a809f7550b716d2abf12
Nicolas Kenner <18199102+nicolaskenner@users.noreply.github.com>
2018-04-12 17:04:10 -0600

    Rename shop constant to store

--- pfaw/constants.py
+++ pfaw/constants.py
@@ -4,6 +4,6 @@ player = 'https://persona-public-service-prod06.ol.epicgames.com/persona/api/pub
 battle_royale = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/stats/accountId/{}/bulk/window/alltime'
 status = 'https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite'
 friends = 'https://friends-public-service-prod06.ol.epicgames.com/friends/api/public/friends/{}'
-shop = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/catalog?rvn={}'
+store = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/catalog?rvn={}'
 news = 'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game'
 profile = 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{}/client/QueryProfile?profileId=athena&rvn={}'
