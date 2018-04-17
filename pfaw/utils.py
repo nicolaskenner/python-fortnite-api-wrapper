9a9fa9c934e49ef4a1212f4054f4327402e2fc4f
Nicolas Kenner <18199102+nicolaskenner@users.noreply.github.com>
2018-04-17 01:38:17 -0600

    Fix formatting in utils.py

--- pfaw/utils.py
+++ pfaw/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def convert_iso_time(isotime):
     """Will convert an isotime (string) to datetime.datetime"""
-    return datetime.strptime(isotime,"%Y-%m-%dT%H:%M:%S.%fZ")
+    return datetime.strptime(isotime, '%Y-%m-%dT%H:%M:%S.%fZ')
