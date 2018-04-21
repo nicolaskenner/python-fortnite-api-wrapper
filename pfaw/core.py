b505c319c99943db0ea3625c2adffdbe7c3b3aef
Nicolas Kenner <18199102+nicolaskenner@users.noreply.github.com>
2018-04-20 21:22:17 -0600

    Add token refresh
    Access token will now update if it nears expiration

--- pfaw/core.py
+++ pfaw/core.py
@@ -1,6 +1,7 @@
-from . import constants
-from . import objects
+from . import constants, objects, utils
+from datetime import datetime, timedelta
 import requests
+import threading
 
 
 class Fortnite:
@@ -18,10 +19,10 @@ class Fortnite:
                                        data={'grant_type': 'exchange_code', 'exchange_code': '{}'.format(code),
                                              'includePerms': True, 'token_type': 'egl'}).json()
 
-        self.access_token = token_response.get('access_token')
-        self.refresh_token = token_response.get('refresh_token')
-        self.expires_at = token_response.get('expires_at')
-        self.session = Session(self.access_token)
+        access_token = token_response.get('access_token')
+        refresh_token = token_response.get('refresh_token')
+        expires_at = utils.convert_iso_time(token_response.get('expires_at'))
+        self.session = Session(access_token, refresh_token, expires_at, fortnite_token)
 
     def player(self, username):
         """Return object containing player name and id"""
@@ -87,24 +88,40 @@ class Fortnite:
 
 
 class Session:
-    def __init__(self, access_token):
+    def __init__(self, access_token, refresh_token, expires_at, fortnite_token):
+        self.refresh_token = refresh_token
+        self.expires_at = expires_at
+        self.fortnite_token = fortnite_token
         self.session = requests.Session()
         self.session.headers.update({'Authorization': 'bearer {}'.format(access_token)})
 
-    def get(self, endpoint, params=None):
-        if params:
-            response = self.session.get(endpoint, params=params)
-        else:
-            response = self.session.get(endpoint)
+        # Check if the access token needs to be updated
+        def check_token():
+            threading.Timer(20.0, check_token).start()
+            now = datetime.utcnow()
+            if self.expires_at < (now - timedelta(seconds=60)):
+                print('Token Refresh')
+                self.refresh()
+
+        check_token()
+
+    def refresh(self):
+        response = requests.post(constants.token, headers={'Authorization': 'basic {}'.format(self.fortnite_token)},
+                                 data={'grant_type': 'refresh_token', 'refresh_token': '{}'.format(self.refresh_token),
+                                       'includePerms': True}).json()
+        access_token = response.get('access_token')
+        self.session.headers.update({'Authorization': 'bearer {}'.format(access_token)})
+        self.refresh_token = response.get('refresh_token')
+        self.expires_at = utils.convert_iso_time(response.get('expires_at'))
+
+    def get(self, endpoint, params=None, headers=None):
+        response = self.session.get(endpoint, params=params, headers=headers)
         if response.status_code != 200:
             response.raise_for_status()
         return response.json()
 
-    def post(self, endpoint, params=None):
-        if params:
-            response = self.session.post(endpoint, params=params)
-        else:
-            response = self.session.post(endpoint)
+    def post(self, endpoint, params=None, headers=None):
+        response = self.session.post(endpoint, params=params, headers=headers)
         if response.status_code != 200:
             response.raise_for_status()
         return response.json()
