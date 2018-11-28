2b68d97db7a074bae9f5a567a45f722d0b1209a2
Nicolas Kenner <18199102+nicolaskenner@users.noreply.github.com>
2018-11-28 13:06:14 -0700

    v0.0.8
    - Requests minimum version 2.20.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fortnite',
-    version='0.0.7',
+    version='0.0.8',
     description='The Python Fortnite API Wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
